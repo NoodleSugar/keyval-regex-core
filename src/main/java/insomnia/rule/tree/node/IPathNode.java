ba5cd5a654773ec4654c6001aa67a0158e9cecb1
zuri <webzuri@gmail.com>
2020-08-19 12:47:53 +0200

    IPathNode, use Optional

--- src/main/java/insomnia/rule/tree/node/IPathNode.java
+++ src/main/java/insomnia/rule/tree/node/IPathNode.java
@@ -1,8 +1,10 @@
 package insomnia.rule.tree.node;
 
+import java.util.Optional;
+
 import insomnia.rule.tree.edge.IEdge;
 
 public interface IPathNode<E> extends ITreeNode<E>
 {
-	IEdge<E> getChild();
+	Optional<? extends IEdge<E>> getChild();
 }
