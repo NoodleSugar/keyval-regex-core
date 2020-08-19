0a84fc17ebb495d3f543db408e97da0cec5fe694
zuri <webzuri@gmail.com>
2020-08-19 12:50:15 +0200

    TreeNode, adapt to interface changes

--- src/main/java/insomnia/rule/tree/node/TreeNode.java
+++ src/main/java/insomnia/rule/tree/node/TreeNode.java
@@ -2,6 +2,7 @@ package insomnia.rule.tree.node;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import insomnia.rule.tree.edge.Edge;
 import insomnia.rule.tree.edge.IEdge;
@@ -9,24 +10,28 @@ import insomnia.rule.tree.edge.IEdge;
 public class TreeNode implements ITreeNode<String>
 {
 	private Edge parent;
+
 	private List<Edge> children;
+
 	private boolean root;
 	private boolean leaf;
-	
+
 	public TreeNode(Edge parent, List<Edge> children, boolean root, boolean leaf)
 	{
 		this.parent = parent;
+
 		this.children = children;
+
 		this.root = root;
 		this.leaf = leaf;
 	}
-	
+
 	@Override
 	public List<Edge> getParents()
 	{
 		ArrayList<Edge> a = new ArrayList<>();
 		a.add(parent);
-		
+
 		return a;
 	}
 
@@ -37,23 +42,8 @@ public class TreeNode implements ITreeNode<String>
 	}
 
 	@Override
-	public IEdge<String> getParent()
+	public Optional<? extends IEdge<String>> getParent()
 	{
-		return parent;
+		return Optional.empty();
 	}
-
-	@Override
-	public boolean isRoot()
-	{
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isLeaf()
-	{
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
