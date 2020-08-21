8006b921ae64d48f807b4ccd2b4164d2c0586c4e
zuri <webzuri@gmail.com>
2020-08-21 16:50:57 +0200

    TestUnifier, simplify code

--- src/test/java/suffixsystem/unifier/TestUnifier.java
+++ src/test/java/suffixsystem/unifier/TestUnifier.java
@@ -3,83 +3,54 @@ package suffixsystem.unifier;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import insomnia.rule.tree.Path;
+import insomnia.rule.tree.Paths;
 import insomnia.suffixsystem.unifier.Unifier;
 
 class TestUnifier
 {
-	private final static String separator = Pattern.quote(".");
-	@ParameterizedTest
-	// b, h, number, [pb, sb, ph, sh, ...]
-	@CsvSource({ //
-			"a.b, b.a.b.a, 3," + " a, , , a.b.a," + " , b, b.a.b, ," + " , , b, a", //
-			"a.a, a.a.a  , 4," + " a, , , a.a," + " , , , a," + " , , a, ," + " , a, a.a, " //
-	})
-	void compute(ArgumentsAccessor args)
+	public static Path pathFromString(String p)
 	{
-		Path body = new Path(args.getString(0).split(separator));
-		Path head = new Path(args.getString(1).split(separator));
-		int  n    = args.getInteger(2).intValue();
-
-		List<Unifier> unifiers = Unifier.compute(body, head);
-		assertEquals(n, unifiers.size());
-
-		ArrayList<String[]> a = new ArrayList<>();
-		for (int i = 0; i < n; i++)
-		{
-			String pb = args.getString(3 + 4 * i + 0);
-			String sb = args.getString(3 + 4 * i + 1);
-			String ph = args.getString(3 + 4 * i + 2);
-			String sh = args.getString(3 + 4 * i + 3);
-
-			a.add(new String[] { pb, sb, ph, sh });
-		}
-
-		for (Unifier u : unifiers)
-			assertTrue(contains(a, u));
+		return Paths.pathFromString(p);
 	}
 
-	private boolean contains(List<String[]> a, Unifier u)
+	public static Unifier unifierFromStrings(String pb, String sb, String ph, String sh)
 	{
-		String pb, sb, ph, sh;
-		if (u.prefixBody == null)
-			pb = null;
-		else
-			pb = StringUtils.join(u.prefixBody.getLabels(), '.');
-		if (u.suffixBody == null)
-			sb = null;
-		else
-			sb = StringUtils.join(u.suffixBody.getLabels(), '.');
-		if (u.prefixHead == null)
-			ph = null;
-		else
-			ph = StringUtils.join(u.prefixHead.getLabels(), '.');
-		if (u.suffixHead == null)
-			sh = null;
-		else
-			sh = StringUtils.join(u.suffixHead.getLabels(), '.');
+		return new Unifier(pathFromString(pb), pathFromString(sb), pathFromString(ph), pathFromString(sh));
+	}
 
-		String[] unif = { pb, sb, ph, sh };
+	static List<Object[]> computeSource()
+	{
+		return Arrays.asList(new Object[][] { //
+				{ pathFromString("a.b"), pathFromString("b.a.b.a"), new Unifier[] { //
+						unifierFromStrings("a", "", "", "a.b.a"), //
+						unifierFromStrings("", "b", "b.a.b", ""), //
+						unifierFromStrings("", "", "b", "a"), //
+				} }, //
+				{ pathFromString("a.a"), pathFromString("a.a.a"), new Unifier[] { //
+						unifierFromStrings("a", "", "", "a.a"), //
+						unifierFromStrings("", "", "", "a"), //
+						unifierFromStrings("", "", "a", ""), //
+						unifierFromStrings("", "a", "a.a", ""), //
+				} }, //
+		});
 
-		loop: for (String[] line : a)
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				if (!(line[i] == null && unif[i] == null //
-					|| line[i] != null && line[i].equals(unif[i])))
-					continue loop;
-			}
-			return true;
-		}
-		return false;
+	}
+
+	@ParameterizedTest
+	@MethodSource("computeSource")
+	void compute(Path body, Path head, Unifier ref_unifiers[])
+	{
+		List<Unifier> unifiers = Unifier.compute(body, head);
+		assertEquals(ref_unifiers.length, unifiers.size());
+
+		for(Unifier ref : ref_unifiers)
+			assertTrue(unifiers.contains(ref));
 	}
 }
