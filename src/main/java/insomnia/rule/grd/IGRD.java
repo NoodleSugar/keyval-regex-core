6c13cef13d62f5cfd01eddc5390bd9ca0f2fef76
zuri <webzuri@gmail.com>
2020-12-22 12:48:25 +0100

    IGRD, add getDependencies()

--- src/main/java/insomnia/rule/grd/IGRD.java
+++ src/main/java/insomnia/rule/grd/IGRD.java
@@ -1,5 +1,7 @@
 package insomnia.rule.grd;
 
+import java.util.Collection;
+
 import org.jgrapht.Graph;
 
 import insomnia.rule.IRule;
@@ -10,4 +12,12 @@ import insomnia.rule.dependency.IDependency;
  */
 public interface IGRD<V, E> extends Graph<IRule<V, E>, IDependency<V, E>>
 {
+	/**
+	 * Get the dependencies of rule with the rules presents in the GRD.
+	 * The rule may not be in the GRD (useful for retrieve dependencies of a query or a data).
+	 * 
+	 * @param rule
+	 * @return
+	 */
+	Collection<IDependency<V, E>> getDependencies(IRule<V, E> rule);
 }
