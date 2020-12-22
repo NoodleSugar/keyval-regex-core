fe6f3174f9fd601a4d78bc1fa19a0006fb225335
zuri <webzuri@gmail.com>
2020-12-22 15:08:17 +0100

    KVValue, implement value

--- src/main/java/insomnia/implem/kv/data/KVValue.java
+++ src/main/java/insomnia/implem/kv/data/KVValue.java
@@ -1,22 +1,65 @@
 package insomnia.implem.kv.data;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 public class KVValue
 {
+	enum Type
+	{
+		NUMBER, STRING, NULL
+	};
+
+	Type   type;
+	Object obj;
 
 	public KVValue()
 	{
+		type = Type.NULL;
+		obj  = ObjectUtils.NULL;
+	}
 
+	public KVValue(Number nb)
+	{
+		type = Type.NUMBER;
+		obj  = nb;
+	}
+
+	public KVValue(String s)
+	{
+		obj = s;
+	}
+
+	public Number getNumber()
+	{
+		assert (type == Type.NUMBER);
+		return (Number) obj;
+	}
+
+	public String getString()
+	{
+		assert (type == Type.STRING);
+		return (String) obj;
 	}
 
 	@Override
 	public String toString()
 	{
-		return "";
+		return obj.toString();
 	}
 
 	@Override
-	public boolean equals(Object obj)
+	public boolean equals(Object object)
 	{
-		return true;
+		if (!(object instanceof KVValue))
+			return false;
+
+		KVValue kvv = (KVValue) obj;
+		return type.equals(kvv.type) && obj.equals(kvv.obj);
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return type.hashCode() + obj.hashCode();
 	}
 }
