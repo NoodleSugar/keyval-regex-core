c2695fc58e74678b0e90eb4485241cfeacef9712
zuri <webzuri@gmail.com>
2021-01-11 17:14:12 +0100

    IPath_as_IFPAPAth

--- src/main/java/insomnia/fsa/fpa/IFPA.java
+++ src/main/java/insomnia/fsa/fpa/IFPA.java
@@ -3,7 +3,6 @@ package insomnia.fsa.fpa;
 import java.util.function.Predicate;
 
 import insomnia.data.IPath;
-import insomnia.implem.fsa.fpa.IPath_as_IFPAElement;
 
 /**
  * E : type of tested elements
@@ -20,6 +19,6 @@ public interface IFPA<VAL, LBL> extends Predicate<IFPAPath<VAL, LBL>>
 
 	default boolean test(IPath<VAL, LBL> path)
 	{
-		return test(IPath_as_IFPAElement.get(path));
+		return test(IPath_as_IFPAPath.get(path));
 	}
 }
--- src/main/java/insomnia/fsa/fpa/IPath_as_IFPAPath.java
+++ src/main/java/insomnia/fsa/fpa/IPath_as_IFPAPath.java
@@ -1,23 +1,22 @@
-package insomnia.implem.fsa.fpa;
+package insomnia.fsa.fpa;
 
 import java.util.List;
 import java.util.Optional;
 
 import insomnia.data.IPath;
-import insomnia.fsa.fpa.IFPAPath;
 
-public class IPath_as_IFPAElement<VAL, LBL> implements IFPAPath<VAL, LBL>
+public class IPath_as_IFPAPath<VAL, LBL> implements IFPAPath<VAL, LBL>
 {
 	private final IPath<VAL, LBL> path;
 
-	private IPath_as_IFPAElement(IPath<VAL, LBL> path)
+	private IPath_as_IFPAPath(IPath<VAL, LBL> path)
 	{
 		this.path = path;
 	}
 
-	public static <VAL, LBL> IPath_as_IFPAElement<VAL, LBL> get(IPath<VAL, LBL> path)
+	public static <VAL, LBL> IPath_as_IFPAPath<VAL, LBL> get(IPath<VAL, LBL> path)
 	{
-		return new IPath_as_IFPAElement<VAL, LBL>(path);
+		return new IPath_as_IFPAPath<VAL, LBL>(path);
 	}
 
 	@Override
