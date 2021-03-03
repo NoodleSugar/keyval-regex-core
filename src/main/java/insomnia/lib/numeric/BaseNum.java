2fbbf6cbb2291755c2646d26b3794b5f14ef3e69
zuri <webzuri@gmail.com>
2021-03-03 19:55:08 +0100

    BaseNum, init

---
+++ src/main/java/insomnia/lib/numeric/BaseNum.java
@@ -0,0 +1,52 @@
+package insomnia.lib.numeric;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+public final class BaseNum
+{
+	private Base  base;
+	private int[] num;
+
+	public BaseNum(BaseNum src)
+	{
+		base = src.base;
+		num  = src.num.clone();
+	}
+
+	public BaseNum(int num, int... base)
+	{
+		this(num, new Base(base));
+	}
+
+	public BaseNum(int num, Base base)
+	{
+		this.base = base;
+		this.num  = new int[base.getBase().length];
+	}
+
+	public void increment()
+	{
+		base.increment(num);
+	}
+
+	public int[] getNum()
+	{
+		return num;
+	}
+
+	public int toInt()
+	{
+		return base.toInt(num);
+	}
+
+	public Base getBase()
+	{
+		return base;
+	}
+
+	@Override
+	public String toString()
+	{
+		return new StringBuilder().append(ArrayUtils.toString(num)).append("(").append(toInt()).append(")").toString();
+	}
+}
